856dca2abdd946c9725ef84acf6448cfcfbcef33
Álex Díaz <adiaz@okode.com>
2017-12-04 14:10:53 +0100

    Generator fixed

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Generator } from './generator';
 export function start(args: any) {
   switch (args[2]) {
     case 'generator':
-    Generator.init(args);
+      Generator.init(args[3], args[4]);
       return;
     default: invalidCommand();
   }
